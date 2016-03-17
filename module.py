89cda3bd2f749f8a7cff8c6e17ffafbd9b68b5b6
Kyrylo <svintsov95@yandex.ru>
2016-03-17 13:36:12 +0200

    2
    - addContact;
    - removeContact;

--- module.py
+++ module.py
@@ -1,1 +1,20 @@
-print ("hello")
+#list of entities
+contactBook = []
+
+def addContact(name,phoneNumber,email):
+    contactEntity  = {}
+    contactEntity["name"] = name
+    contactEntity["phoneNumber"] = phoneNumber
+    contactEntity["email"] = email
+    contactBook.__add__(contactEntity)
+
+def removeContact(name):
+    for contact in contactBook:
+        if contact["name"] == name:
+            print("Contact with name "+name+" was deleted")
+            contactBook.remove(contact)
+            return
+    else:
+        print ("Contact with name "+name+ " was not found")
+
+
